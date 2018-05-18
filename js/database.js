2de34cb69ccef027c500d66d64bf7dc6266dee4b
pepijn <pepijn.mommersteeg@gmail.com>
2018-05-18 04:02:15 +0200

    d

--- js/database.js
+++ js/database.js
@@ -1,22 +1,34 @@
 class Database {
-  ip;
-  api_key;
-  socket;
   constructor(ip, api_key) {
     this.ip = ip;
     this.api_key = api_key;
+    this.xhttp = new XMLHttpRequest();
+    this.webapi="http://strategoavans.herokuapp.com/";
   }
-  function connect()
+  connect()
   {
     if(!(this.socket)){
-      this.socket = io.connect('https://strategoavans.herokuapp.com/', {query: 'api_key=' + this.api_key});
+      this.socket = io.connect(this.webapi, {query: 'api_key=' + this.api_key});
     }
   }
-  function userinfo()
+  userInfo()
   {
-    if(!(this.socket)){
-      this.socket = io.connect('https://strategoavans.herokuapp.com/', {query: 'api_key=' + this.api_key});
+    return post('api/users/me',{});
+  }
+  deleteGames()
+  {
+    return delete('api/games',{});
+  }
+  gameList()
+  {
+    return get('api/games',{});
+  }
+  newGame(ai = false)
+  {
+    if(this.ai){
+
     }
+    return post('api/games',{});
   }
   this.socket.on('connect', function() {
     console.log('Connected')
@@ -32,6 +44,26 @@ class Database {
   this.socket.on('error', function(error) {
       console.error(error);
   });
-  this.privatefunction = function(name) {  };
+  this.post = function(url,data) {
+    this.url=webapi+this.url+'?api_key='+api_key;
+    this.xhttp.open("POST", this.url, true);
+    this.xhttp.setRequestHeader("Accept", "application/json");
+    array.send(JSON.stringify(this.data));
+    return xhttp.responseText;
+  };
+  this.get = function(url,data) {
+    this.url=this.webapi+this.url+'?api_key='+api_key;
+    this.xhttp.open("GET", this.url, true);
+    this.xhttp.setRequestHeader("Accept", "application/json");
+    array.send(JSON.stringify(this.data));
+    return xhttp.responseText;
+  };
+  this.delete = function(url,data) {
+    this.url=webapi+this.url+'?api_key='+api_key;
+    this.xhttp.open("DELETE", this.url, true);
+    this.xhttp.setRequestHeader("Accept", "application/json");
+    array.send(JSON.stringify(this.data));
+    return xhttp.responseText;
+  };
 
 }
