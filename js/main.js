09a564fbd1498dffc7ed5b670715f5b5c2f95352
pepijn <pepijn.mommersteeg@gmail.com>
2018-05-18 12:09:52 +0200

    KUT JS

--- js/main.js
+++ js/main.js
@@ -1,5 +1,40 @@
 class Main {
+
   constructor(){
+    var lobby = new Lobby();
+    var input = document.querySelector("#loginInput");
+    var click = document.querySelector("#loginClick");
+    var main;
+    var apiKey;
+    var login = function() {
+      apiKey= input.value;
+      start();
+    }
+  }
+  test(){
+    login();
+  }
+  main(){
+    //create html
+    input.addEventListener("keyup", function(event) {
+      //on  enter start login
+      if (event.keyCode === 13) {
+        login();
+      }
+    });
+    click.addEventListener("click", function(event) {
+        login();
+    });
 
   }
+
+  start(){
+    document.querySelector("#login").classList.add("hide");
+    document.querySelector("#lobby").classList.remove("hide");
+  }
+
 }
+document.addEventListener("DOMContentLoaded", function() {
+  main = new Main();
+  main.main();
+});
